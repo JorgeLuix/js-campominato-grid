16715eb32a13b4a0eed242c9baf5927980716244
JorgeLuix <tec.jorge.29@gmai.com>
2023-03-19 16:18:52 +0100

    aggiuta fx per removere eventi, revealsBomb,

--- campominato/campo.js
+++ campominato/campo.js
@@ -14,6 +14,8 @@ function drawSquare(index, sidenumSquares) {
   square.innerHTML = index;
   return square;
 }
+
+//funzione numeri Randomici
 function getRndNumber(max) {
   return Math.floor(Math.random() * max);
 }
@@ -38,6 +40,7 @@ function play(e) {
 
   const NUM_BOMBS = 16;
   let gameover = false;
+  let firstBombIndex = -1;
 
 
   const level = document.getElementById("level").value;
@@ -55,57 +58,74 @@ function play(e) {
       break;
   }
 
-
-  let squareforRow = Math.sqrt(squareNumbers);
-  const bombs = generateBombs(NUM_BOMBS, squareNumbers);
-  console.log("🚀 ~ file: campo.js:61 ~ play ~ bombs:", bombs)
-
-  
   function checkWin() {
     const blueSquares = document.querySelectorAll(".sq-blue");
     const numBlueSquares = blueSquares.length;
     const numSafeSquares = squareNumbers - NUM_BOMBS;
     if (numBlueSquares === numSafeSquares) {
+      removeSquareListeners();
       alert("Complimenti, hai vinto!");
     }
   }
+  
+  //Funzione per mostrare le posizione delle bombe
+  function revealBombs() {
+    const squares = document.querySelectorAll(".square");
+    for (let i = 0; i < squares.length; i++) { //--> Se un quadrato contiene una bomba, alla casella viene aggiunta la classe .sq-red
+      if (bombs.indexOf(i + 1) !== -1) {
+        squares[i].classList.add("sq-red");
+        squares[i].innerHTML = `<i class="fas fa-bomb"></i>`;
+      }
+    }
+  }
+
+
+  function removeSquareListeners() {
+    const squares = document.querySelectorAll(".square");
+    for (let i = 0; i < squares.length; i++) {
+      squares[i].removeEventListener("click", squareClickHandler);
+    }
+  }
+
+  function squareClickHandler() {
+    const index = parseInt(this.innerHTML); //per modificare il contenuto del quadrato(square)
+    const isBomb = bombs.indexOf(index) !== -1; //controllo di INDEX nel array(bombs)
+
+    if (isBomb) { // -> = true
+      this.classList.add("sq-red");
+      this.innerHTML = `<i class="fas fa-bomb"></i>`;
+
+      if (firstBombIndex === -1) { // -> 
+        firstBombIndex = index;
+      } else {
+        revealBombs();
+        removeSquareListeners(); //-> evita che il gioco vada avanti una volta trovata una bomba
+        gameover = true;
+        alert("Game over!");
+      }
+    } else {
+      this.classList.add("sq-blue");
+      if (!gameover) {
+        checkWin();
+      }
+    }
+  }
+
+
+  let squareforRow = Math.sqrt(squareNumbers);
+  const bombs = generateBombs(NUM_BOMBS, squareNumbers);
+  console.log("🚀 ~ file: campo.js:61 ~ play ~ bombs:", bombs)
  
 
   for (let i = 1; i <= squareNumbers; i++) {
     const isBomb = bombs.indexOf(i) !== -1;
-    const square = drawSquare(i, squareforRow, isBomb);
-    square.addEventListener("click", function () {
+    const square = drawSquare(i, squareforRow);
 
-      if (isBomb) {
-        square.classList.add("sq-red");
-        square.innerHTML = isBomb ? `<i class="fas fa-bomb"></i>` : index;
-        gameover = true;
-        alert("Game over!");
-      } else {
-        square.classList.add("sq-blue");
-      }
-      if (!gameover) {
-        checkWin();
-      }
-     /* if (bombs.includes(i)) {
-        console.log("🚀 ~ file: campo.js:67 ~ bombs:", bombs)
-        square.classList.add("sq-red")
-        square.innerHTML = isBomb ? `<i class="fas fa-bomb"></i>` : index;
-        alert("GAME OVER!");
-      } else {
-        square.classList.add("sq-blue");
-      }*/
-    
-    });
 
-    playground.appendChild(square);
-  }
- /* function checkWin() {
-    const blueSquares = document.querySelectorAll(".sq-blue");
-    const numBlueSquares = blueSquares.length;
-    const numSafeSquares = squareNumbers - NUM_BOMBS;
-    if (numBlueSquares === numSafeSquares) {
-      alert("Congratulations, you won!");
+    if (!gameover) {
+      square.addEventListener("click", squareClickHandler);
     }
-  }*/
-}
+  
+      playground.appendChild(square);
+    }
+  }
